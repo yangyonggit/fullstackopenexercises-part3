63020c07fafcbfe632998e7b9932071204a1ea49
yangyonggit <byangyong@gmail.com>
2024-04-22 16:45:41 +0800

    -add phone validator , 3.20

--- models/persondb.js
+++ models/persondb.js
@@ -24,6 +24,12 @@ const personSchema = new mongoose.Schema({
     },
     number: {
       type: String,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d{7,8}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
       required: true,
     }
   })
